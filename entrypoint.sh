bc1bfc35f4a5c798f01a28640388dcbfb5f6462d
akshzt <akyvzz@gmail.com>
2025-03-25 13:03:16 +0530

    Use unbuffered logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Execute application
-exec python3 -m src.main --host 0.0.0.0 --port 8765 --certfile "${CERT_FILE}" --keyfile "${KEY_FILE}" "$@"
+exec python3 -u -m src.main --host 0.0.0.0 --port 8765 --certfile "${CERT_FILE}" --keyfile "${KEY_FILE}" "$@"
